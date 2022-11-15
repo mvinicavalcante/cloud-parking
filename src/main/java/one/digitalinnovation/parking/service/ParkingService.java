d0b2ea8a5d950fb0eb1c3993fcd80cd7163d1217
Marcos Vinícius Cavalcante Oliveira <mvinic2011@gmail.com>
2022-11-15 00:35:00 -0300

    Aplicando padrões arquiteturais

---
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -0,0 +1,31 @@
+package one.digitalinnovation.parking.service;
+
+import one.digitalinnovation.parking.controller.dto.ParkingDTO;
+import one.digitalinnovation.parking.model.Parking;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class ParkingService {
+
+    private static Map<String, Parking> parkingMap = new HashMap();
+
+    static {
+        var id = getUUID();
+        Parking parking = new Parking(id, "DMS-1111", "SC", "CELTA", "PRETO");
+        parkingMap.put(id, parking);
+    }
+
+    private static String getUUID() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public List<Parking> findAll() {
+        return parkingMap.values().stream().collect(Collectors.toList());
+    }
+}
