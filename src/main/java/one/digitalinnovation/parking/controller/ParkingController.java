110d55edb4d58ae91cc2b246ed27fe474161a5a8
Marcos Vinícius Cavalcante Oliveira <mvinic2011@gmail.com>
2022-11-18 15:26:54 -0300

    Persistencia e erros solucionados

--- src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
+++ src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
@@ -69,7 +69,7 @@ public class ParkingController {
 
     @PostMapping("/{id}")
     public  ResponseEntity<ParkingDTO> exit(@PathVariable String id) {
-        Parking parking = parkingService.exit(id);
+        Parking parking = parkingService.checkOut(id);
         return ResponseEntity.ok(parkingMapper.toParkingDTO(parking));
     }
 
